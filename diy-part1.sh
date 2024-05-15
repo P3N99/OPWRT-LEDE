4bd90fb8668cd2007cb177db78c9642cf6078fa4
P3N99 <145282517+P3N99@users.noreply.github.com>
2024-05-15 18:11:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,8 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 cd package
 sed -i "s/OpenWrt /P3N9 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.3.1/g' base-files/files/bin/config_generate
+
+# 替换argon主题
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
